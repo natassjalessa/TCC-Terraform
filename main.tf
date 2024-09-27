742131310d74f3e48e78b623823f97ced1122ceb
Natassja Lessa <90574399+natassjalessa@users.noreply.github.com>
2024-09-27 19:59:31 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,6 +66,16 @@ resource "azurerm_linux_virtual_machine" "tcc" {
     username   = var.admin_username
     public_key = var.ssh_public_key
   }
-
 }
+
+resource "azurerm_storage_account" "tcc" {
+  name                      = var.storage_account_name
+  resource_group_name       = azurerm_resource_group.main.name
+  location                  = var.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+
+  allow_blob_public_access  = false
+  min_tls_version           = "TLS1_2"
+  enable_https_traffic_only = true
 
